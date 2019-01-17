9f59a668bbfeca5519f996d8ff80c15d64cce023
Mitchell Henke <mitchh23@gmail.com>
2019-01-17 09:50:00 -0600

    fix phoenix warning in development

--- config/config.exs
+++ config/config.exs
@@ -20,7 +20,7 @@ use Mix.Config
 #
 #     config :logger, level: :info
 #
-
+config :phoenix, :json_library, Jason
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
